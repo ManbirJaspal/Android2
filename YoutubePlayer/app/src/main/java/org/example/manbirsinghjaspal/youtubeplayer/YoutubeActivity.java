a8ee5128399bef3200cbe2cdc0b8a6fc23697ac0
manbirsinghjaspal <manb.jaspal@gmail.com>
2018-07-26 13:18:53 -0400

    YoutubeApp

---
+++ YoutubePlayer/app/src/main/java/org/example/manbirsinghjaspal/youtubeplayer/YoutubeActivity.java
@@ -0,0 +1,39 @@
+package org.example.manbirsinghjaspal.youtubeplayer;
+
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+
+public class YoutubeActivity extends YouTubeBaseActivity
+        implements YouTubePlayer.OnInitializedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_youtube);
+//        ConstraintLayout constraintLayout = findViewById(R.id.activity_youtube);
+        ConstraintLayout layout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_youtube, null);
+        setContentView(layout);
+
+        Button button1 = new Button(this);
+        button1.setLayoutParams(new ConstraintLayout.LayoutParams(300, 80));
+        button1.setText("Button added");
+        layout.addView(button1);
+
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+    }
+}
