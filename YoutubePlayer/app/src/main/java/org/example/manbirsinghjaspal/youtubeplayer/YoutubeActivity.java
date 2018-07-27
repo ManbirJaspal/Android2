0e9913215a035d202629d8d95f5c6b24852c2155
manbirsinghjaspal <manb.jaspal@gmail.com>
2018-07-27 12:23:05 -0400

    Youtube app almost done

--- YoutubePlayer/app/src/main/java/org/example/manbirsinghjaspal/youtubeplayer/YoutubeActivity.java
+++ YoutubePlayer/app/src/main/java/org/example/manbirsinghjaspal/youtubeplayer/YoutubeActivity.java
@@ -1,29 +1,38 @@
 package org.example.manbirsinghjaspal.youtubeplayer;
 
-import android.support.constraint.ConstraintLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.support.constraint.ConstraintLayout;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 public class YoutubeActivity extends YouTubeBaseActivity
         implements YouTubePlayer.OnInitializedListener {
+    public static final String GOOGLE_API_KEY = "AIzaSyA63qOF2es_pViUFTd1LI_u8xjLfxNxKew";
+    static final String YOUTUBE_VIDEO_ID = "ElpitAfkRS4";
+    static final String YOUTUBE_PLAYLIST = "PLXtTjtWmQhg1SsviTmKkWO5n0a_-T0bnD";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_youtube);
-//        ConstraintLayout constraintLayout = findViewById(R.id.activity_youtube);
+//        ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.activity_youtube);
         ConstraintLayout layout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_youtube, null);
         setContentView(layout);
 
-        Button button1 = new Button(this);
-        button1.setLayoutParams(new ConstraintLayout.LayoutParams(300, 80));
-        button1.setText("Button added");
-        layout.addView(button1);
+//        Button button1 = new Button(this);
+//        button1.setLayoutParams(new ConstraintLayout.LayoutParams(300, 80));
+//        button1.setText("Button added");
+//        layout.addView(button1);
+
+        YouTubePlayerView playerView = new YouTubePlayerView(this);
+        playerView.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        layout.addView(playerView);
+        playerView.initialize(GOOGLE_API_KEY, this);
 
     }
 
@@ -34,6 +43,14 @@ public class YoutubeActivity extends YouTubeBaseActivity
 
     @Override
     public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        final int REQUEST_CODE = 1;
+
+        if (youTubeInitializationResult.isUserRecoverableError()) {
+            youTubeInitializationResult.getErrorDialog(this, REQUEST_CODE).show();
+        } else {
+            String errorMessage = String.format("There was an erroe initialzing youtube Player (1%s)", youTubeInitializationResult.toString());
+            Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
+        }
 
     }
 }
